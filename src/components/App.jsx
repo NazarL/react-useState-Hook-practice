928d50ad02b0c396d3ec6aff4ffccc922330306a
NazarL <nazar.lebedenko@gmail.com>
2021-03-17 18:35:26 -0400

    set automatic time update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 function App() {
+  setInterval(updateTime, 1000);
+
   let now = new Date().toLocaleTimeString();
   var [time, setTime] = useState(now);
   console.log();
