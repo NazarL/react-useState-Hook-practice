a59352d2660e92e9cba7caf700f13af699187a26
NazarL <nazar.lebedenko@gmail.com>
2021-03-17 18:30:31 -0400

    update local time on button press

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  let now = new Date().toLocaleTimeString();
+  var [time, setTime] = useState(now);
+  console.log();
+
+  function updateTime() {
+    let newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
